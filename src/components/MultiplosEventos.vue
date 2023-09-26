c3568edc9d00f4417f27763f72403f0b512f09be
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-09-26 10:00:43 -0300

    aula 15

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+
+        <button @click="primeiro(txt1,$event), segundo(txt2,$event),terceiro()">ativar múltiplos eventos</button>
+        <p>{{ eventos }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return {
+            eventos: "",
+            txt1:"Primeiro evento",
+            txt2:"Segundo evento"
+        }
+    },
+    methods: {
+        primeiro(txt,e) {
+            this.eventos = txt
+        },
+        segundo(txt,e) {
+            setTimeout(() => this.eventos = txt,500)
+        },
+        terceiro(){
+            setTimeout(() => this.eventos = "último evento",2500)
+        }
+    }
+}
+
+</script>
+<style scoped>
+div {
+    margin: 100px;
+}
+</style>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,15 +3,18 @@
     <h1>Olá Vue!</h1>
     <p>Eu me chamo {{ nome }} e sou {{ profissao }}</p>
     <Info />
+    <MultiplosEventos />
   </div>
 </template>
 
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue";
 import Info from "./Info.vue";
 export default {
   name: `PrimeiroComponente`,
   components: {
     Info,
+    MultiplosEventos
   },
   data() {
     return {
