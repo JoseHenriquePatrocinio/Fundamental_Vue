d5a6ff76b1d8e248ba713b2604119410e225a4f9
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-09-25 23:45:55 -0300

    aula 13

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,12 +2,13 @@
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Estou em busca de novas oportunidades</p>
-    <p>Utilizo as seguintes tecnologias:</p>
+    <p>Utilizo as seguintes tecnologias para backend:</p>
     <ul>
-      <li>Javascript</li>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>C++</li>
+      <li v-for="(technology, index) in backendTech" v-bind:key="index">{{ technology }}</li>
+    </ul>
+    <p>Utilizo as seguintes tecnologias para frontend:</p>
+    <ul>
+      <li v-for="technology in frontendTech" v-bind:key="technology.id">{{ technology.lang }}</li>
     </ul>
     <div>
       <button @click="showEmail">{{textoBotao}}</button>
@@ -31,7 +32,9 @@ export default {
       mostrar_email: false,
       email: "henriquepatrocinio98@gmail.com",
       meu_link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      textoBotao:"Mostrar e-mail"
+      textoBotao:"Mostrar e-mail",
+      backendTech: ['Javascript','Python','C#','C++'],
+      frontendTech:[{id:1,lang:'Vue'},{id:2,lang:'HTML'},{id:3,lang:'CSS'}]
     }
   },
   methods:{
